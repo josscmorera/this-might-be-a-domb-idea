a2e0146a563d093c550ce376ebfa8208248eefb2
josscmorera <me@jossmorera.com>
2023-03-09 13:59:39 -0500

    end.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let para = document.querySelector("p");
 para.style.color = "lightblue"; // Change the font color of the paragraph to light blue ✅
 para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."; // Change the text of the paragraph to be the following Hipster Ipsum ✅
 
-
 let header = document.querySelector("h1");
 header.style.fontSize = "48px"; // Change the font size of the heading to 48px ✅
 
