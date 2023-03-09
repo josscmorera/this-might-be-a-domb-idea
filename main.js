817de735a7a0bef0eb18c11dfbb9b0706517c3b4
josscmorera <me@jossmorera.com>
2023-03-09 13:50:10 -0500

    end

---
+++ main.js
@@ -0,0 +1,24 @@
+let para = document.querySelector("p");
+para.style.color = "lightblue"; // Change the font color of the paragraph to light blue ✅
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."; // Change the text of the paragraph to be the following Hipster Ipsum ✅
+
+
+let header = document.querySelector("h1");
+header.style.fontSize = "48px"; // Change the font size of the heading to 48px ✅
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = "0.5"; // Change the 13th item to be half transparent. (Look this up!) ✅
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = 'I say, "Hi!"'; // Change the 3rd item's text to say: "I say, "Hi!"" ✅
+
+let img = document.querySelector("img");
+img.src = "https://cdn.shopify.com/s/files/1/2663/7914/files/goofy-face2_large.png?v=1528926935"; // Change the image below the list to be whatever image you want. ✅
+img.style.height = "300px"; // Change the image below the list to be whatever image you want. ✅
+
+let myImage = document.querySelector("#second-image");
+myImage.src = "https://static.tvtropes.org/pmwiki/pub/images/toppngcom_oofy_walking_goofy_mickey_mouse_371x556.png";
+myImage.style.height = "300px"; // Now make that image 300 pixels high (with JS) ✅
+
+let item16 = document.querySelector("#item-16"); // Now, (with JS) give it the same class as the others ✅
+item16.innerText = "Won't get fooled again."; // Give that <li> the following text: Won't get fooled again ✅
